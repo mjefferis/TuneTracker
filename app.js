e97cb5647b96c34fc01191c5150ac460a8a891ff
Dennis <bereznd1@tcnj.edu>
2018-03-19 19:56:58 -0400

    firebase

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
+// Sets up the way that the various elements on the page animate
 $('#formy').hide();
 $('#successform').hide();
 $('#emptyform').hide();
-
-$('#upload').on('click', function () {
-    event.preventDefault();
-    $('#formy').show();
-    $('#upload').hide();
-
-});
 
 
 //this is an example of velocity.js this website is a good tutorial: http://www.independent-software.com/velocity-js-tutorial-accelerated-javascript-animation/
@@ -18,3 +12,72 @@ $("#upload").velocity({ translateY: 125 }, {
     duration: 2250,
     easing: [ 300, 8 ]
   });
+
+
+
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC9fF0OACaRvyDOUZcLG5bX_LEUgmK6yGo",
+    authDomain: "tunetracker-2260d.firebaseapp.com",
+    databaseURL: "https://tunetracker-2260d.firebaseio.com",
+    projectId: "tunetracker-2260d",
+    storageBucket: "tunetracker-2260d.appspot.com",
+    messagingSenderId: "474472193819"
+  };
+  
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+
+
+$('#upload').on('click', function (event) {
+    event.preventDefault();
+    $('#formy').show();
+    $('#upload').hide();
+
+});
+
+
+
+$("#submit").on("click", function (event) {
+
+     event.preventDefault();
+
+    var name = $("#user-name").val().trim();
+    var contact = $("#user-contact").val().trim();
+    var fileURL = $("#user-file").val().trim();
+    var desc = $("#user-description").val().trim();
+
+    var newPost = {
+        name: name,
+        contact: contact,
+        fileURL: fileURL,
+        desc: desc
+    };
+
+    database.ref().push(newPost);
+
+    // Clears all of the text-boxes
+    $("#user-name").val("");
+    $("#user-contact").val("");
+    $("#user-file").val("");
+    $("#user-description").val("");
+
+    //Hides the form & shows the upload button again
+
+    $('#formy').hide();
+    $('#upload').show();
+
+
+
+});
+
+
+
+
+
+
