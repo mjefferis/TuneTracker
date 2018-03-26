efad7b37da9a529bda9ef1a9ae1c4a1e4bb58e44
Dennis <bereznd1@tcnj.edu>
2018-03-26 18:24:45 -0400

    changes

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-// Sets up the way that the various elements on the page animate
+//Hides the form from appearing on the page upon first load
 $('#formy').hide();
 $('#successform').hide();
 $('#emptyform').hide();
 
 
-//this is an example of velocity.js this website is a good tutorial: http://www.independent-software.com/velocity-js-tutorial-accelerated-javascript-animation/
+// Velocity JS Library (controls the animation of the page's elements)
 $(".jumbotron").velocity("fadeIn", { duration: 1500 })
-
-//more velocity js
 $("#upload").velocity({ translateY: 125 }, {
     duration: 2250,
     easing: [300, 8]
+});
+
+// Code for how the form pops up when Upload is clicked
+$('#upload').on('click', function (event) {
+    event.preventDefault();
+    $('#formy').show();
+    $('#upload').hide();
+
+});
+
+//Code for what happens when the cancel button is clicked
+$('#cancel-btn').on('click', function (event) {
+    event.preventDefault();
+    $('#formy').hide();
+    $('#upload').show();
+
+    //Clears all of the text-boxes
+    $("#user-name").val("");
+    $("#user-contact").val("");
+    $("#user-file").val("");
+    $("#user-description").val("");
 });
 
 
 
 
 
-// Initialize Firebase
+// Initializes Firebase
 var config = {
     apiKey: "AIzaSyC9fF0OACaRvyDOUZcLG5bX_LEUgmK6yGo",
     authDomain: "tunetracker-2260d.firebaseapp.com",
@@ -34,63 +53,29 @@ var database = firebase.database();
 
 
 
-$('#upload').on('click', function (event) {
-    event.preventDefault();
-    $('#formy').show();
-    $('#upload').hide();
-
-});
 
 
-$('#cancel-btn').on('click', function (event) {
-    event.preventDefault();
-    $('#formy').hide();
-    $('#upload').show();
-
-    // Clears all of the text-boxes
-    $("#user-name").val("");
-    $("#user-contact").val("");
-    $("#user-file").val("");
-    $("#user-description").val("");
-});
-
-
-
+// Sets up what happens when the submit button is clicked on the form
 $("#submit").on("click", function (event) {
 
     event.preventDefault();
 
-
+    //Takes in the values that were entered in the form
     var name = $("#user-name").val().trim();
     var contact = $("#user-contact").val().trim();
     var fileURL = $("#user-file").val().trim();
     var desc = $("#user-description").val().trim();
 
 
-/*
-    if (navigator.geolocation) {
-        var lat_lng = navigator.geolocation.getCurrentPosition(function(position){
-          var user_position = {};
-          user_position.lat = position.coords.latitude; 
-          user_position.lng = position.coords.longitude; 
-          callback(user_position);
-        });
-    }
-*/
-
-
 
     var x = document.getElementById("coords");
 
+
+    //Runs a function to acquire the user's coordinates when they are submitting a new post
     function getLocation(callback) {
         if (navigator.geolocation) {
             var lat_lng = navigator.geolocation.getCurrentPosition(function (position) {
-                //var user_position = [position.coords.longitude, position.coords.latitude];
                 var lng = position.coords.longitude;
-                //user_position.lat = position.coords.latitude; 
-                // user_position.lng = position.coords.longitude; 
-                
-
                 var lat = position.coords.latitude;
                 callback(lng + ", " + lat);
             });
@@ -103,27 +88,7 @@ $("#submit").on("click", function (event) {
         }
     }
 
-
-    function getLocation(callback) {
-        if (navigator.geolocation) {
-            var lat_lng = navigator.geolocation.getCurrentPosition(function (position) {
-                //var user_position = [position.coords.longitude, position.coords.latitude];
-                var lng = position.coords.longitude;
-                //user_position.lat = position.coords.latitude; 
-                // user_position.lng = position.coords.longitude; 
-                
-
-                var lat = position.coords.latitude;
-                callback(lng + ", " + lat);
-            });
-
-        }
-
-
-        else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
-        }
-    }
+    //Runs the getLocation function to push the values from the form to Firebase, as an object
     getLocation(function (lat_lng) {
 
         database.ref().push({
@@ -136,46 +101,6 @@ $("#submit").on("click", function (event) {
         });
 
     });
-
-
-
-    /*
-     
-        function getLocation() {
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(showPosition);
-            } else {
-                x.innerHTML = "Geolocation is not supported by this browser.";
-            }
-        }
-     
-        function showPosition(position) {
-            x.innerHTML = "[" + position.coords.latitude + "," + position.coords.longitude + "]";
-            //window.coordsGlobal = coords;
-            //return coords;
-            //console.log(coords);
-     
-        }
-     
-     
-        getLocation();
-    */
-
-
-    /*
-    var newPost = {
-        name: name,
-        contact: contact,
-        fileURL: fileURL,
-        desc: desc,
-        //coords: coords
-    
-    
-    };
-    
-    database.ref().push(newPost);
-    */
-
 
 
     // Clears all of the text-boxes
@@ -192,49 +117,6 @@ $("#submit").on("click", function (event) {
 
 
 });
-
-
-
-
-    //TRIED TO PUT THE FUNCTION IN AN OBJECT
-    /*
-        var location = {
-    
-            getLocation: function () {
-                if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(this.showPosition);
-                } else {
-                    x.innerHTML = "Geolocation is not supported by this browser.";
-                }
-            },
-    
-    
-            showPosition: function (position) {
-                var coords = "[" + position.coords.latitude + "," + position.coords.longitude + "]";
-                return coords;
-                console.log(coords);
-                //window.coordsGlobal = coords;
-                //console.log(coords2);
-            },
-    
-    
-            coords: position.coords.latitude
-    
-        }
-    */
-
-
-
-
-
-        /*
-    geoFire.set(<any-name-identifier>, [<lat>,<long>]).then(function() {
-        console.log("Location added")
-        }).catch(function(error) {
-        console.log(error);
-        });
-*/
-
 
 
 
