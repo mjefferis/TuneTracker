51e14f46f1b6bfe54c92b499af0c4ece28e744ee
Dennis <bereznd1@tcnj.edu>
2018-03-26 19:52:58 -0400

    most of app working

--- app.js
+++ app.js
@@ -2,30 +2,38 @@
 $('#formy').hide();
 $('#successform').hide();
 $('#emptyform').hide();
+$('#badlink').hide();
 
-
-// Velocity JS Library (controls the animation of the page's elements)
+//Velocity JS for animations
 $(".jumbotron").velocity("fadeIn", { duration: 1500 })
-$("#upload").velocity({ translateY: 125 }, {
+$("#map").velocity("fadeIn", { duration: 2500 })
+$("#upload").velocity({ translateY: 520 }, {
     duration: 2250,
     easing: [300, 8]
 });
+$("body").velocity({
+    backgroundColor: "#00004d"
+}, {
+        duration: 2500,
+        easing: "easeInQuad"
+    });
 
-// Code for how the form pops up when Upload is clicked
+//Code for what happens when you hit the Upload button - form pops up
 $('#upload').on('click', function (event) {
     event.preventDefault();
-    $('#formy').show();
+    var slideDir = $('#formy').is(':visible') ? 'slideUp' : 'slideDown';
+    $('#formy').velocity(slideDir);
     $('#upload').hide();
-
+    $('#map').hide();
 });
 
-//Code for what happens when the cancel button is clicked
+//Code for what happens when you hit the Cancel button once you're already typing inside the form
 $('#cancel-btn').on('click', function (event) {
     event.preventDefault();
     $('#formy').hide();
     $('#upload').show();
-
-    //Clears all of the text-boxes
+    $('#map').show();
+    // Clears all of the text-boxes
     $("#user-name").val("");
     $("#user-contact").val("");
     $("#user-file").val("");
@@ -54,67 +62,288 @@ var database = firebase.database();
 
 
 
-
-// Sets up what happens when the submit button is clicked on the form
-$("#submit").on("click", function (event) {
-
-    event.preventDefault();
-
-    //Takes in the values that were entered in the form
-    var name = $("#user-name").val().trim();
-    var contact = $("#user-contact").val().trim();
-    var fileURL = $("#user-file").val().trim();
-    var desc = $("#user-description").val().trim();
-
-
-
-    var x = document.getElementById("coords");
+//Generates the map
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2hhbGlsb3dlbnM5MiIsImEiOiJjamV2bDR0aXQ3NDdrMzlvNzFjbGw1MHI4In0.Rj8983ke7W9GO3QnOLJg8A';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [-75.0, 39.0],//latLng.split(','), // starting position
+    zoom: 6 // starting zoom
+}).addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}));
 
 
-    //Runs a function to acquire the user's coordinates when they are submitting a new post
-    function getLocation(callback) {
-        if (navigator.geolocation) {
-            var lat_lng = navigator.geolocation.getCurrentPosition(function (position) {
-                var lng = position.coords.longitude;
-                var lat = position.coords.latitude;
-                callback(lng + ", " + lat);
+
+
+
+//Sets up what happens when the map loads (adding map markers & popups)
+map.on('load', function () {
+
+    //Empty array where popup info will go
+    var popUps = [];
+
+    //Firebase watcher, gets run every time a new child is added
+    database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+
+        //Firebase watcher, is run once to be able to use the value of the response
+        database.ref().once('value', function (snapshot) {
+
+            //Runs code for each child within the overall snapshot
+            snapshot.forEach(function (childSnapshot) {
+
+                if (!childSnapshot.val().coords) {
+                    return;
+                }
+                
+
+                //Gets the relavant info for the popups from firebase
+                var name = childSnapshot.val().name;
+                var contact = childSnapshot.val().contact;
+                var fileURL = childSnapshot.val().fileURL;
+                var desc = childSnapshot.val().desc;
+                var coords = childSnapshot.val().coords;
+                coords = coords.split(',');
+
+
+
+                var player;
+                function onYouTubeIframeAPIReady() {
+    
+                    var fileURL = childSnapshot.val().fileURL;
+                    var userVideoID = getId(fileURL);
+    
+                    player = new YT.Player('ytPlayer', {
+                        height: '100%',
+                        width: '100%',
+                        videoId: userVideoID,
+                    });
+    
+    
+                }
+
+
+                
+
+
+                //JSON code for Mapbox popups
+                var formattedData = {
+                    "type": "Feature",
+                    "properties": {
+                        "description": "<table><tr><td><h1>" + name + "</h1></td></tr><tr><td>" + contact + "</td></tr><tr><td><div id='ytPlayer'> </div></td></tr><tr><td>" + desc + "</td</tr></table>",
+                        "icon": "theatre"
+                    },
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": coords
+                    }
+                };
+
+                //Adds the code for each popup to the array
+                popUps.push(formattedData);
+
             });
 
-        }
+            console.log(popUps);
+
+            //Adds a map layer which uses the popUps array we filled up previously
+            map.addLayer({
+                "id": "places",
+                "type": "symbol",
+                "source": {
+                    "type": "geojson",
+                    "data": {
+                        "type": "FeatureCollection",
+                        "features": popUps
+                    }
+                },
+                "layout": {
+                    "icon-image": "{icon}-15",
+                    "icon-allow-overlap": true
+                }
+            });
 
 
-        else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
-        }
-    }
 
-    //Runs the getLocation function to push the values from the form to Firebase, as an object
-    getLocation(function (lat_lng) {
 
-        database.ref().push({
-            name: name,
-            contact: contact,
-            fileURL: fileURL,
-            desc: desc,
-            coords: lat_lng
+
+
+
+
+
+
+
+
+
 
         });
 
     });
 
 
-    // Clears all of the text-boxes
-    $("#user-name").val("");
-    $("#user-contact").val("");
-    $("#user-file").val("");
-    $("#user-description").val("");
-
-    //Hides the form & shows the upload button again
-
-    $('#formy').hide();
-    $('#upload').show();
+    // When a click event occurs on a feature in the places layer, open a popup at the
+    // location of the feature, with description HTML from its properties.
+    map.on('click', 'places', function (e) {
+        var coordinates = e.features[0].geometry.coordinates.slice();
+        var description = e.features[0].properties.description;
 
 
+        // Ensures that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+
+    // Changes the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'places', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+    // Changes it back to a pointer when it leaves.
+    map.on('mouseleave', 'places', function () {
+        map.getCanvas().style.cursor = '';
+    });
+
+
+});
+
+
+
+
+function getId(url) {
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+
+    var match = url.match(regExp);
+
+    if (match && match[2].length == 11) {
+        return match[2];
+    } else {
+        return "Error: Can't get Video ID";
+    }
+
+};
+
+
+
+
+
+
+
+// Sets up what happens when the submit button is clicked on the form
+$("#submit").on("click", function (event) {
+
+    event.preventDefault();
+
+
+    // player = new YT.Player('ytPlayer', {
+    //     height: '100%',
+    //     width: '100%',
+    //     videoId: userVideoID,
+    // });
+
+    //Takes in the values that were entered in the form
+    var name = $("#user-name").val().trim();
+    var contact = $("#user-contact").val().trim();
+    var fileURL = $("#user-file").val().trim();
+    var userVideoID = getId(fileURL);
+    var desc = $("#user-description").val().trim();
+
+
+
+
+
+    function validateYouTubeUrl(url) {
+        if (url != undefined || url != '') {
+            var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=|\?vi=)([^#\&\?]*).*/;
+            var match = url.match(regExp);
+            if (match && match[2].length == 11) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+
+
+
+
+    if (name === "" || contact === "" || fileURL === "" || desc === "") {
+        $('#emptyform').show();
+        setTimeout(function () { $("#emptyform").hide(); }, 4000);
+    }
+
+    else if (!validateYouTubeUrl(fileURL)) {
+        $('#badlink').show();
+        setTimeout(function () { $("#badlink").hide(); }, 4000);
+    }
+
+
+    else {
+
+
+
+        var x = document.getElementById("coords");
+
+
+        //Runs a function to acquire the user's coordinates when they are submitting a new post
+        function getLocation(callback) {
+            if (navigator.geolocation) {
+                var lat_lng = navigator.geolocation.getCurrentPosition(function (position) {
+                    var lng = position.coords.longitude;
+                    var lat = position.coords.latitude;
+                    callback(lng + ", " + lat);
+                });
+
+            }
+
+
+            else {
+                x.innerHTML = "Geolocation is not supported by this browser.";
+            }
+        }
+
+        //Runs the getLocation function to push the values from the form to Firebase, as an object
+        getLocation(function (lat_lng) {
+
+            database.ref().push({
+                name: name,
+                contact: contact,
+                fileURL: fileURL,
+                desc: desc,
+                coords: lat_lng
+
+            });
+
+        });
+
+
+        // Clears all of the text-boxes
+        $("#user-name").val("");
+        $("#user-contact").val("");
+        $("#user-file").val("");
+        $("#user-description").val("");
+
+        //Hides the form & shows the upload button again
+
+        $('#formy').hide();
+        $('#map').show();
+        $('#successform').show();
+        setTimeout(function () { $("#successform").hide(); }, 3000);
+        setTimeout(function () { $("#upload").show(); }, 3000);
+
+
+
+    }
 
 });
 
