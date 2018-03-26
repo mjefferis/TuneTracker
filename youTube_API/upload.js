67353677a9a4edf953a01d1f1819e42c62131c03
Dennis Ruto <rutodeno@gmail.com>
2018-03-25 22:56:52 -0400

    enabled youtube api to work

--- youTube_API/upload.js
+++ youTube_API/upload.js
@@ -1,33 +1,41 @@
 
+
+
+// function get takes in a link and gets out the videoID
 function getId(url) {
     var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
 
     var match = url.match(regExp);
 
     if (match && match[2].length == 11){
-
         return match[2];
     } else {
         return "error";
     }
 
 }
+ 
+var apiKey = 'AIzaSyDj5D3uV6x8m2T6OKI8Ku7DOeddJkP8JwQ';
+  
+var player;
+function onYouTubeIframeAPIReady() {
+    $("#click-me").on("click", function (event) {
 
-$("#submit").on("click", function(event){
+        event.preventDefault();
 
-    event.preventDefault();
+        var rawLink = $("#user-file").val().trim();
+        var userVideoID = getId(rawLink);
 
-    var rawLink =$("#link").val().trim();
-     var embedLink = getId(rawLink);
-     console.log(embedLink);
+        player = new YT.Player('ytPlayer', {
+            height: '360',
+            width: '640',
+            videoId: userVideoID,
 
-     $("#link").emmpty();
+
+        });
+    });
+
+};
     
 
-
-
-});
-
-
-
-
+    
